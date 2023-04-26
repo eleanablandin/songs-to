ddf99fb5f92637b1902b31e27833d7c074e33aac
Eleana Peña <eleanablandin@gmail.com>
2023-04-26 16:31:50 -0400

    changin routes for deezer_app.route, import Blueprint

--- deezer.py
+++ deezer.py
@@ -1,23 +1,19 @@
 from flask import Flask, redirect, request, url_for, jsonify
 from flask_oauthlib.client import OAuth
-import requests
+from flask import Blueprint
 
 
-
-app = Flask(__name__)
-
-flask_key = open('flask_key.txt', 'r').read().splitliens()
-app.secret_key = flask_key[0]
+deezer_app = Blueprint('deezer_app', __name__)
 
 
 #Deezer API keys 
-deezer_keys = open('deezer.txt', 'r').read().splitlines
+deezer_keys = open('deezer.txt', 'r').read().splitlines()
 id_key = deezer_keys[0]
 secret_key = deezer_keys[1]
 
 
 #Deezer crendential settings 
-deezer = OAuth(app).remote_app(
+deezer = OAuth(deezer_app).remote_app(
     'deezer',
     consumer_key=id_key,
     consumer_secret=secret_key,
@@ -30,13 +26,13 @@ deezer = OAuth(app).remote_app(
 
 
 #Login fot the auth 
-@app.route('/login')
+@deezer_app.route('/login')
 def login():
     callback_url = request.url_root + 'callback'
     return deezer.authorize(callback=callback_url or None)
 
 #Function to get token 
-@app.route('/callback')
+@deezer_app.route('/callback')
 def callback():
     resp = deezer.authorized_response()
 
@@ -51,10 +47,10 @@ def callback():
     with open('access_token.txt', 'w') as file:
         file.write(access_token)
     
-    return redirect(url_for('home', access_token=access_token))
+    return redirect(url_for('deezer_app.home', access_token=access_token))
 
 
-@app.route('/')
+@deezer_app.route('/')
 def home():
     with open('access_token.txt', 'r') as file:
         access_token = file.read()
@@ -62,6 +58,6 @@ def home():
     if access_token:
         return 'Token access: {}'.format(access_token)
     else:
-         return 'Please, authorize your application in <a href="/login">login</a>'
+        return 'Please, authorize your application in <a href="/login">login</a>'
 
 
